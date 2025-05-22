9df116a603829a5d2ea9d3f174e23adf8429276b
Micka N <m.nouhaud@icloud.com>
2025-05-22 22:41:44 +0200

    1er bloc ok : 5 barres de données

---
+++ js/cours.js
@@ -0,0 +1,6 @@
+let montantCash = document.querySelector(".montant-cash");
+let montantBank = document.querySelector(".montant-bank");
+let montantDebt = document.querySelector(".montant-debt");
+let montantGuns = document.querySelector(".montant-guns");
+let barreVie = document.querySelector(".barre-vie")
+let pourcentageVie = document.querySelector(".pour-cent");
