898cfba4c24954be4692122ec50e53029f04abef
Micka N <m.nouhaud@icloud.com>
2025-05-25 09:19:53 +0200

    Changé en 'Street Deals', à ajuster correctement + ombres boutons + A RETRECIR PARTIE HAUTE

--- js/cours.js
+++ js/cours.js
@@ -2,5 +2,20 @@ let montantCash = document.querySelector(".montant-cash");
 let montantBank = document.querySelector(".montant-bank");
 let montantDebt = document.querySelector(".montant-debt");
 let montantGuns = document.querySelector(".montant-guns");
-let barreVie = document.querySelector(".barre-vie")
+let barreVie = document.querySelector(".barre-vie");
 let pourcentageVie = document.querySelector(".pour-cent");
+
+let cash = 2000;
+let bank = 0;
+let debt = 5500;
+let guns = 0;
+let health = 100;
+
+function mettreAJourStatut() {
+    montantCash.textContent = cash;
+    montantBank.textContent = bank;
+    montantDebt.textContent = debt;
+    montantGuns.textContent = guns; 
+    pourcentageVie.textContent = `${health}%`;
+    barreVie.style.width = `${health}%`;
+}
